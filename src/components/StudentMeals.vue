bb530027ac0b33074ce5e439caab048e58ca50bc
matia-gov <mohamed.atia@phila.gov>
2020-03-31 13:57:57 -0400

    linting

--- src/components/StudentMeals.vue
+++ src/components/StudentMeals.vue
@@ -1,151 +1,105 @@
 <template>
-
   <div>
-    <div
-      v-show="loading"
-      class="mtm center"
-    >
+    <div v-show="loading" class="mtm center">
       <i class="fas fa-spinner fa-spin fa-3x" />
     </div>
-  <div
-      v-show="failure"
-      class="h3 mtm center"
-    >
-      Sorry, there was a problem. Please try again.
-    </div>
+    <div v-show="failure" class="h3 mtm center">Sorry, there was a problem. Please try again.</div>
     <div v-show="!loading && !failure">
-    <h5><b>District schools open Mondays and Thursdays, 9 a.m. – noon.</b></h5>
-    <p>Families will receive six meals per student—3 breakfasts and 3 lunches</p>
-     <div class="search">
-      <input
-        id="district-search-bar"
-        v-model="districtSearch"
-        class="search-field"
-        type="text"
-        placeholder="Filter by site name or address"
-      ><input
-        ref="district-search-bar"
-        type="submit"
-        class="search-submit"
-        value="Search"
-      >
-      <button
-        v-if="districtSearch.length > 0"
-        class="clear-search-btn"
-        @click="clearDisctrictSearch()"
-      >
-        <i class="fas fa-times " />
-      </button>
-    </div>
-    <div
-      v-show="!loading && districtEmpty"
-      class="h3 mtm center"
-    >
-      Sorry, there are no results.
-    </div>
-    <table
-     v-show="!districtEmpty && !loading && !failure"
-     >
-      <thead>
-        <tr>
-          <th
-            class="table-sort title"
-            :class="districtSort"
-            @click="toggleDist()"
-          >
-            <span>School</span>
-          </th>
-          <th>Address (ZIP code)</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr v-for="site in filteredDistrict" 
-          :key="site.OBJECTID">
-          <td>{{site.SiteName }}</td>
-         <td>{{ site.ADDRESS }} ({{ site.ZipCode}})</td>
-      </tr>
-      </tbody>
-    </table>
-    <h5><b>Charter School locations open on varying days from 9 a.m. to noon. (contact schools or visit their websites for exact days)</b></h5>
-    <div class="search">
-      <input
-        id="charter-search-bar"
-        v-model="charterSearch"
-        class="search-field"
-        type="text"
-        placeholder="Filter by site name or address"
-      ><input
-        ref="charter-search-bar"
-        type="submit"
-        class="search-submit"
-        value="Search"
-      >
-      <button
-        v-if="charterSearch.length > 0"
-        class="clear-search-btn"
-        @click="clearCharterSearch()"
-      >
-        <i class="fas fa-times " />
-      </button>
-    </div>
-    <div
-      v-show="!loading && charterEmpty"
-      class="h3 mtm center"
-    >
-      Sorry, there are no results.
-    </div>
-     <table
-      v-show="!charterEmpty && !loading && !failure"
-     >
-      <thead>
-        <tr>
-          <th
-            class="table-sort title"
-            :class="charterSort"
-            @click="toggleCharter()"
-          >
-            <span>School</span>
-          </th>
-          <th>Address (ZIP code)</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr v-for="site in filteredCharter" 
-          :key="site.OBJECTID">
-          <td>{{site.SiteName }}</td>
-         <td>{{ site.ADDRESS }} ({{ site.ZipCode}})</td>
-      </tr>
-      </tbody>
-    </table>
-    <h5><b>The Philadelphia Housing Authority has six community centers open for “grab-and-go” meals for breakfast and lunch from Monday through Friday, 9 a.m. to noon.</b></h5>    <div
-      v-show="!loading && PHAEmpty"
-      class="h3 mtm center"
-    >
-      Sorry, there are no results.
-    </div>
-     <table
-      v-show="!PHAEmpty && !loading && !failure"
-     >
-      <thead>
-        <tr>
-          <th
-            class="table-sort title"
-            :class="PHASort"
-            @click="togglePHA()"
-          >
-            <span>PHA community center</span>
-          </th>
-          <th>Address (ZIP code)</th>
-        </tr>
-      </thead>
-      <tbody>
-      <tr v-for="site in filteredPHA" 
-          :key="site.OBJECTID">
-          <td>{{site.SiteName }}</td>
-         <td>{{ site.ADDRESS }} ({{ site.ZipCode}})</td>
-      </tr>
-      </tbody>
-    </table>
+      <h5>
+        <b>District schools open Mondays and Thursdays, 9 a.m. – noon.</b>
+      </h5>
+      <p>Families will receive six meals per student—3 breakfasts and 3 lunches</p>
+      <div class="search">
+        <input
+          id="district-search-bar"
+          v-model="districtSearch"
+          class="search-field"
+          type="text"
+          placeholder="Filter by site name or address"
+        />
+        <input ref="district-search-bar" type="submit" class="search-submit" value="Search" />
+        <button
+          v-if="districtSearch.length > 0"
+          class="clear-search-btn"
+          @click="clearDisctrictSearch()"
+        >
+          <i class="fas fa-times" />
+        </button>
+      </div>
+      <div v-show="!loading && districtEmpty" class="h3 mtm center">Sorry, there are no results.</div>
+      <table v-show="!districtEmpty && !loading && !failure">
+        <thead>
+          <tr>
+            <th class="table-sort title" :class="districtSort" @click="toggleDist()">
+              <span>School</span>
+            </th>
+            <th>Address (ZIP code)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="site in filteredDistrict" :key="site.OBJECTID">
+            <td>{{site.SiteName }}</td>
+            <td>{{ site.ADDRESS }} ({{ site.ZipCode}})</td>
+          </tr>
+        </tbody>
+      </table>
+      <h5>
+        <b>Charter School locations open on varying days from 9 a.m. to noon. (contact schools or visit their websites for exact days)</b>
+      </h5>
+      <div class="search">
+        <input
+          id="charter-search-bar"
+          v-model="charterSearch"
+          class="search-field"
+          type="text"
+          placeholder="Filter by site name or address"
+        />
+        <input ref="charter-search-bar" type="submit" class="search-submit" value="Search" />
+        <button
+          v-if="charterSearch.length > 0"
+          class="clear-search-btn"
+          @click="clearCharterSearch()"
+        >
+          <i class="fas fa-times" />
+        </button>
+      </div>
+      <div v-show="!loading && charterEmpty" class="h3 mtm center">Sorry, there are no results.</div>
+      <table v-show="!charterEmpty && !loading && !failure">
+        <thead>
+          <tr>
+            <th class="table-sort title" :class="charterSort" @click="toggleCharter()">
+              <span>School</span>
+            </th>
+            <th>Address (ZIP code)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="site in filteredCharter" :key="site.OBJECTID">
+            <td>{{site.SiteName }}</td>
+            <td>{{ site.ADDRESS }} ({{ site.ZipCode}})</td>
+          </tr>
+        </tbody>
+      </table>
+      <h5>
+        <b>The Philadelphia Housing Authority has six community centers open for “grab-and-go” meals for breakfast and lunch from Monday through Friday, 9 a.m. to noon.</b>
+      </h5>
+      <div v-show="!loading && PHAEmpty" class="h3 mtm center">Sorry, there are no results.</div>
+      <table v-show="!PHAEmpty && !loading && !failure">
+        <thead>
+          <tr>
+            <th class="table-sort title" :class="PHASort" @click="togglePHA()">
+              <span>PHA community center</span>
+            </th>
+            <th>Address (ZIP code)</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="site in filteredPHA" :key="site.OBJECTID">
+            <td>{{site.SiteName }}</td>
+            <td>{{ site.ADDRESS }} ({{ site.ZipCode}})</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   </div>
 </template>
@@ -153,15 +107,15 @@
 <script>
 import axios from "axios";
 import VueFuse from "vue-fuse";
-import Vue from 'vue';
+import Vue from "vue";
 
 Vue.use(VueFuse);
 
-const endpoint = 
-"https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Youth_Meal_Sites_3_20_2020_public_test/FeatureServer/0/query?where=1%3D1&outFields=*&f=pjson";
+const endpoint =
+  "https://services.arcgis.com/fLeGjb7u4uXqeF9q/arcgis/rest/services/Youth_Meal_Sites_3_20_2020_public_test/FeatureServer/0/query?where=1%3D1&outFields=*&f=pjson";
 
 export default {
-  name: 'StudentMeals',
+  name: "StudentMeals",
   data: function() {
     return {
       mealSites: [],
@@ -190,43 +144,46 @@ export default {
         shouldSort: true,
         threshold: 0.1,
         tokenize: true,
-        keys: [
-          'SiteName',
-          'ADDRESS',
-          'ZipCode'
-        ],
-      },
-      
-    }
+        keys: ["SiteName", "ADDRESS", "ZipCode"]
+      }
+    };
   },
   watch: {
     districtSearch(val) {
-      this.filteredDistrict = this.searchTable(this.districtSites, "district", val)
+      this.filteredDistrict = this.searchTable(
+        this.districtSites,
+        "district",
+        val
+      );
     },
 
     charterSearch(val) {
-      this.filteredCharter = this.searchTable(this.charterSites, "charter", val)
+      this.filteredCharter = this.searchTable(
+        this.charterSites,
+        "charter",
+        val
+      );
     },
 
     PHASearch(val) {
-      this.filteredPHA = this.searchTable(this.PHASites, "PHA", val)
+      this.filteredPHA = this.searchTable(this.PHASites, "PHA", val);
     },
 
     filteredDistrict() {
       if (!this.failure) {
         if (this.filteredDistrict.length == 0) {
           this.districtEmpty = true;
-        } else  {
+        } else {
           this.districtEmpty = false;
         }
       }
     },
 
-     filteredCharter() {
+    filteredCharter() {
       if (!this.failure) {
         if (this.filteredCharter.length == 0) {
           this.charterEmpty = true;
-        } else  {
+        } else {
           this.charterEmpty = false;
         }
       }
@@ -236,67 +193,75 @@ export default {
       if (!this.failure) {
         if (this.filteredPHA.length == 0) {
           this.PHAEmpty = true;
-        } else  {
+        } else {
           this.PHAEmpty = false;
         }
       }
-    },
-
+    }
   },
   mounted() {
-    this.fetchData()
+    this.fetchData();
   },
   methods: {
     fetchData: function() {
       this.loading = true;
-      axios.get(endpoint)
-      .then((response) => {
-        this.mealSites = response.data.features;
+      axios
+        .get(endpoint)
+        .then(response => {
+          this.mealSites = response.data.features;
 
-        response.data.features.forEach((item)=> {
-          this.mealSites.push(item.attributes);
+          response.data.features.forEach(item => {
+            this.mealSites.push(item.attributes);
+          });
+          this.failure = false;
+          this.loading = false;
+
+          this.splitIntoArrays();
         })
-        this.failure = false;
-        this.loading = false;
-
-        this.splitIntoArrays();
-      })
-      .catch((e) => {
-        console.log(e)
-        this.loading = false;
-        this.failure = true;
-      })
+        .catch(e => {
+          console.log(e);
+          this.loading = false;
+          this.failure = true;
+        });
     },
 
     toggleDist() {
-      this.districtSort = this.districtSort === 'asc' ? 'desc' : 'asc';
-      this.filteredDistrict = this.sortSites(this.filteredDistrict, this.districtSort)
+      this.districtSort = this.districtSort === "asc" ? "desc" : "asc";
+      this.filteredDistrict = this.sortSites(
+        this.filteredDistrict,
+        this.districtSort
+      );
     },
 
     toggleCharter() {
-      this.charterSort = this.charterSort === 'asc' ? 'desc' : 'asc';
-      this.filteredCharter = this.sortSites(this.filteredCharter, this.charterSort)
+      this.charterSort = this.charterSort === "asc" ? "desc" : "asc";
+      this.filteredCharter = this.sortSites(
+        this.filteredCharter,
+        this.charterSort
+      );
     },
 
     togglePHA() {
-      this.PHASort = this.PHASort === 'asc' ? 'desc' : 'asc';
-      this.filteredPHA = this.sortSites(this.filteredPHA, this.PHASort)
+      this.PHASort = this.PHASort === "asc" ? "desc" : "asc";
+      this.filteredPHA = this.sortSites(this.filteredPHA, this.PHASort);
     },
 
-    sortSites:function(toSort, mod) {
+    sortSites: function(toSort, mod) {
       return toSort.sort((a, b) => {
-         let modifier = -1;
-          if(mod === 'desc') {
-            modifier = 1;
-          }
+        let modifier = -1;
+        if (mod === "desc") {
+          modifier = 1;
+        }
         var textA = a.SiteName.toUpperCase();
         var textB = b.SiteName.toUpperCase();
-          return (textA < textB) ? ( -1 * modifier) : (textA > textB) ? (1 * modifier) : 0;
+        return textA < textB ? -1 * modifier : textA > textB ? 1 * modifier : 0;
       });
     },
 
-    splitIntoArrays: function () {
-      let districtArr = this.mealSites.filter(site => site.Status === "District");
+    splitIntoArrays: function() {
+      let districtArr = this.mealSites.filter(
+        site => site.Status === "District"
+      );
       this.districtSites = this.sortSites(districtArr, this.districtSort);
       this.filteredDistrict = this.districtSites;
 
@@ -335,15 +300,15 @@ export default {
 
     clearPHASearch: function() {
       this.PHASearch = "";
-    },
-  },
-}
+    }
+  }
+};
 </script>
 
 <style scoped>
 .clear-search-btn {
   position: absolute;
-  top:16px;
+  top: 16px;
   right: 70px;
   padding: 0;
   font-size: 20px;
